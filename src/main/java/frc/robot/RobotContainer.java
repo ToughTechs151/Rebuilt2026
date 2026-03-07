77375984ddd16e454bb76fb696636b211e48ff64
SimplyOrdinary <goodbot2007@gmail.com>
2026-03-07 13:50:04 -0500

    Added a scale factor to the POV forward turning for easier use. Fixes #31

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -100,7 +100,8 @@ public class RobotContainer {
   // Commands to shift robot position at low speed using POV
   SwerveInputStream shiftForwardRobotOriented =
       SwerveInputStream.of(drivebase.getSwerveDrive(), () -> DriveConstants.POV_SPEED, () -> 0.0)
-          .withControllerRotationAxis(() -> -DriverStation.getStickAxis(0, 4))
+          .withControllerRotationAxis(
+              () -> -(DriverStation.getStickAxis(0, 4) * DriveConstants.POV_TURNING))
           .robotRelative(true)
           .allianceRelativeControl(false);
 
