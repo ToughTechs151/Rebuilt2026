252e0a55486e3e8635cbe2dae2990521824dd366
jwt388 <jtnh388@gmail.com>
2026-03-11 10:16:07 -0400

    Add command and fix missing requirements

--- src/main/java/frc/robot/Game.java
+++ src/main/java/frc/robot/Game.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Supplier;
 
 /** The Game class contains functions specific to the game. */
@@ -170,6 +171,6 @@ public class Game {
           // Return command to drive
           return drivebase.driveToPosePID(approachPose, launchPose);
         },
-        null);
+        Set.of(drivebase));
   }
 }
